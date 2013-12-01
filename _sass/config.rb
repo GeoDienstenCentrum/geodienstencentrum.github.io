254060fdd947320c5ed6e6be32129d0e49044711
GeoDienstenCentrum <github@geodienstencentrum.nl>
2013-12-01 14:35:54 +0100

    eenvoudige tag list, icomoon symbolen

--- _sass/config.rb
+++ _sass/config.rb
@@ -6,10 +6,11 @@ sass_dir = "./"
 css_dir = "../css"
 images_dir = "../img"
 javascripts_dir = "../js"
+fonts_dir = "../fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
